cb58023f82cb5429228faec042326e1812a43bc4
joeymc406 <98559082+joeymc406@users.noreply.github.com>
2022-03-10 22:42:51 -0700

    adding files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+alert('Hello World');
+
+let myName = 'bob';
+document.write(myName);
+
+myName = 'john doe';
+document.write(myName);
+
+let size = '100';
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+let favoriteFood = 'ribeye steak';
+document.write(favoriteFood);
+
+//keep practicing and check in about the local clone of my repository
+//on to the next
