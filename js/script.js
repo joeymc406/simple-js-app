dda1aaa18e20a70cd3db467b5d7ba20b962911d1
joeymc406 <98559082+joeymc406@users.noreply.github.com>
2022-03-16 00:31:40 -0600

    added task 1.2 changes to  script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-alert('Hello World');
+let  pokemonList = [{
+    name: 'bulbasaur'
+    height: 1
+    type: unknown
 
-let myName = 'bob';
-document.write(myName);
-
-myName = 'john doe';
-document.write(myName);
-
-let size = '100';
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-let favoriteFood = 'ribeye steak';
-document.write(favoriteFood);
-
-//keep practicing and check in about the local clone of my repository
-//on to the next
+    name: 'charizard'
+    height: 1
+    type: unknown
+    
+    name: 'squirtle'
+    height: 1
+    type: unknown
+}]
