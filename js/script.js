096c15d2d9b4fd1d86b0e7de3a07a9c273d4df6c
joeymc406 <98559082+joeymc406@users.noreply.github.com>
2022-03-20 12:13:14 -0600

    created array and print function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-let pokemonList = [{
-    name: 'bulbasaur',
-        height: 1,
-        type: unknown
-},
-{
-    name: 'charizard',
-        height: 1,
-        type: unknown
-},
-{
-    name: 'squirtle',
-        height: 1,
-        type: unknown
-}
-]
+//creating pokemon array
+et pokemonList = [];
+pokemonList[0] = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: [grass, poison]
+};
+pokemonList[1] = {
+    Name: 'Squirtle',
+    height: 0.5,
+    type: ['water'],
+};
+pokemonlist[2] = {
+    name: 'Charmander',
+    height: 0.6,
+    type: ['fire, flying']
+};
+//print size and name of pokemon to the page.
+for( let i = 0; i < pokemonList.length; i++){
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`)
+};
