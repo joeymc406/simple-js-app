7b1bfb9566501c273bee10370a44d1e519e05bf2
joeymc406 <98559082+joeymc406@users.noreply.github.com>
2022-04-01 21:36:43 -0600

    added iife and console.log + add pikachu function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
+let pokemonRepository= (function() {
+// Adding of IIFE in the form of the pokemonRepository above and at the end of the array
 //creating pokemon array
-let pokemonList = [
-    {
+    let pokemonList = [
+        {
         name: 'Bulbasaur',
         height: 0.7,
         type: ['grass', 'poison']
-    },
-    {
+        },
+         {
         name: 'Squirtle',
         height: 0.5,
         type: ['water'],
-    },
-    {
-    name: 'Charmander',
-    height: 0.6,
-    type: ['fire', 'flying']
-    }
-];
-
+         },
+         {
+         name: 'Charmander',
+        height: 0.6,
+        type: ['fire', 'flying']
+        }
+    ];
+    return {
+        add: function(pokemon){
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+     }
+ })();
+//added console.log at end of additon of pokemonRepository IIFE
+ console.log(pokemonRepository.getAll());
+ pokemonRepository.add({ name: 'Pikachu'});
+ console.log(pokemonReposityr.getAll());
 //print size and name of pokemon to the page.
 
 //switching of for funtion for the forEach function
