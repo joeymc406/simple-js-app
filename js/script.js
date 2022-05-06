c422fc885f74ddc44185d88128d6408a4bb93a67
joeymc406 <98559082+joeymc406@users.noreply.github.com>
2022-05-06 00:28:04 -0600

    removed unused code. followed order from other student.

--- js/script.js
+++ js/script.js
@@ -65,6 +65,8 @@ let pokemonRepository = (function() {
         }   
 // load list functions added.
     function loadList() {
+        showLoadingMessage();
+
         return fetch(apiUrl).then(function (response) {
         return response.json();
              }).then(function (json) {
@@ -79,124 +81,40 @@ let pokemonRepository = (function() {
                 });
         }).catch(function (e) {
             console.error(e);
-        })
+            hideLoadingMessage();
+        });
     }
 //load details functions added
     function loadDetails(item) {
-        let url = item.detailsUrl;
+        showLoadingMessage();
+
+    let url = item.detailsUrl;
             return fetch(url).then(function (response) {
                 return response.json();
             }).then(function (details) {
                 item.imageUrl = details.sprites.front_default;
                 item.height = details.height;
+                item.weight = details.weight;
                 item.types = details.types;
+                hideLoadingMessage();
             }).catch(function (e) {
                 console.error(e);
+                hideLoadingMessage();
             });
-    }
-
-        }
-//returns all items from functions
-    return  {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem,
-      loadDetails: loadDetails,
-      showDetails: showDetails,
-      loadList: loadList
-    };
-})();
-
-    
-    // modals added to IIFE.
-
-        function showModal(title, text) {
-            //clear all existing modal content
-            modalContainer.innerHTML = ' ';
-    
-        let modal = document.createElement('div');
-            modal.classList.add('modal');
-    
-            //add the new modal content
-        let closeButtonElement = document.createElement('button');
-            closeButtonElement.classList.add('modal-close');
-            closeButtonElement.innerText = 'close';
-            closeButtonElement.addEventListener('click', hideModal);
-
-        let titleElement = document.createElement('h1');
-            titleElement.innerText = title;
-    
-        let contentElement = document.createElement('p');
-            contentElement.innerText = text;
-    
-            modal.appendChild(closeButtonElement);
-            modal.appendChild(titleElement);
-            modal.appendChild(contentElement);
-            modalContainer.appendChild(modal);
-
-            modalContainer.classList.add('is-visible');
         }
 
-        // dialog promise reject variable used and called later in the return promise function
-        // return promise reject function shortened by using this variable. by calling it later in the promise return
-        // allows modal to always show rejected when closed any way it is closed
-
-        let dialogPromiseReject;// can be set later, bu showDialog???
-        //hide modal function
-        function hideModal() {
-            modalContainer.classList.remove('is-visible');
-
-        if (dialogPromiseReject) {
-            dialogPromiseReject();
-            dialogPromiseReject = null;
-            }
-        }
-
-        // dialog modal
-        function showDialog(title, text) {
-            showModal(title, text);
-
-            //modalContainer defined here
-        let modalContainer = document.querySelector('#modal-container');
-
-            //addition of confirmation/cancel button to modal
-        let Modal = modalContainer.querySelector('.modal');
-
-        let confirmButton = document.createElement('button');
-            confirmButton.classList.add('modal-confirm');
-            confirmButton.innerText = 'Confirm';
-        
-        let cancelButton = document.createElement('button');
-            cancelButton.classList.add('modal-cancel');
-            cancelButton.innerText = 'Cancel';
-
-            modal.appendChild(confirmButton);
-;           modal.appendChild(cancelButton);
-
-            // focus of the confurm button so user can simply press enter
-            confirmButton.focus();
-        }
-
-        document.querySelector('show-modal').addEventListener('click', () => {
-            showModal('Modal Title', 'This is the modal content');
+        document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal('modal Title', 'Modal Content');
         });
 
-        document.querySelector('#show-dialog').addEventListener('click', () => {
-            showDialog('Confirm action', 'Are you sure you want to do this').then(function() {
-                alert('confirmed!');
-            }, () => {
-                alert('not confirmed');
-            });
-        });
-
-          // escape key scenario
+      // escape key scenario
         window.addEventListener('keydown', (e) => {
-            let modalContainer = document.querySelector('#modal-container');
-            if (e.key == 'Escape' && modalContainer.classList.contains('is-visible')) {
-                hideModal();
-            }
-        });
-    
+        // escape key executes hide modal function
+        if (e.key == 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+
         modalContainer.addEventListener('click', (e) =>
         {
             let target = e.target;
@@ -205,17 +123,76 @@ let pokemonRepository = (function() {
             }
         });
 
-        //promise return function of resolve and reject for dialog modal
-        return new Promise((resolve, reject) => {
-            cancelButton.addEventListener('click', hideModal);
-            confirmButton.addEventListener('click', () => {
-                dialogPromiseReject = null; // reset this????
-                hideModal();
-                resolve();
-            });
-            dialogPromiseReject = reject;
-        });   
-})();
+    function showModal(pokemon) {
+            //clear all existing modal content
+            modalContainer.innerHTML = '';
+    
+    let modal = document.createElement('div');
+            modal.classList.add('modal');
+    
+            //add the new modal content
+    let closeButtonElement = document.createElement('button');
+            closeButtonElement.classList.add('modal-close');
+            closeButtonElement.innerText = 'close';
+            closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+            titleElement.innerText = pokemon.name; 
+
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'height:' + pokemon.height;
+
+    let weightElement = document.createElement('p');
+        typeElement.innerText = 'Types: ';
+        pokemon.types.forEach((type, numberOfTypes) => {
+        numberOfTypes = pokemon.types.pokemon;
+        
+        if (numberOfTypes ===1) {
+            typeElement.innerText += type.type.name;
+        } else {
+            typeElement.innerText += type.type.name + " ";
+        }
+    })
+    
+    let imageElement = document.createElement('img');
+    imageElement.classList.add(modal-image);
+    imageElement.src = pokemon.imageUrl
+    // modals added to IIFE.
+
+    
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(imageElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(heightElement)
+            modal.appendChild(weightElement);
+            modal.appendChild(typeElement);
+            modal.appendChild(contentElement);
+            modalContainer.appendChild(modal);
+
+            // adds .is-visible to modal container
+            modalContainer.classList.add('is-visible');
+        }
+
+        //hide modal function
+        function hideModal() {
+            modalContainer.classList.remove('is-visible');
+
+        // dialog modal
+        function showDialog(title, text) {
+            showModal(title, text);
+        }
+    }
+
+//returns all items from functions
+return  {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
+    loadList: loadList
+  };
+});
 
 
 // replaced pokemon.list and replaced with pokemon.repository for the iife
@@ -224,8 +201,3 @@ pokemonRepository.getAll().forEach(function(pokemon) {
 pokemonRepository.addListItem(pokemon);
     });
 });
-
-
-
-
-// Addition of pokemon.repository IIFE.
