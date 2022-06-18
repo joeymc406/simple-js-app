5ce8060e0df4e6b5416dd2b6b658d340bafe46a1
joeymc406 <98559082+joeymc406@users.noreply.github.com>
2022-06-17 20:33:51 -0600

    types edited improper code

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ let pokemonRepository = (function () {
     let pokemonImage = $(`<img class="modal-img mx-auto" src="${pokemonlogo.png}" alt="pokemon Logo" ${pokemon.name}">`);
     let pokemonHeight = $(`<p class="ml-4 mt-2 mb-0">Height: ${pokemon.height}</p>`);
     let pokemonWeight = $(`<p class="ml-4 mb-0">Weight: ${pokemon.weight}</p>`);
-    let pokemonTypes = $(`<p class="ml-4">types: ${pokemon.types.join(', ')}</p>`);
+    let pokemonTypes = $(`<p class="ml-4">types: ${pokemon.types.map(item => item.type.name).join(', ')}</p>`);
 
     
     // Append all pokemon elements.
